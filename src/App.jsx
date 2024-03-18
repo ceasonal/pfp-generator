c1d9a67845e402a54867cca11aedb6a5f569c31d
ceabenshawn <ceasonalgamer@gmail.com>
2024-03-18 16:55:12 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Images from "./Images";
 import Navabar from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/react"
 const App = () => {
   return (
     <>
       <Router>
+        <Analytics/>
         <Navabar />
         <Routes>
           <Route path="/" element={<Home />} />
